9c60b9ff3b27f867b51d4e26652ee6d973764d9d
MinJe Kim <121341025+chase1281@users.noreply.github.com>
2025-09-25 19:06:50 +0900

    Update User.java
    code review

--- week3/src/main/java/com/example/BackendBeginner/domain/User.java
+++ week3/src/main/java/com/example/BackendBeginner/domain/User.java
@@ -3,7 +3,7 @@ package com.example.BackendBeginner.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter //도메인을 쓸 때 Setter를 사용하면 안 좋다. 이유는? 다른 사람이 마음대로 바꿀 수 있기 때문.
 public class User {
     private String username;
     private Long userId;
