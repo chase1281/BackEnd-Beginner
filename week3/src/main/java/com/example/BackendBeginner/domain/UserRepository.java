4891793d8733fc92ae998b225a92ad9c8f4d2810
MinJe Kim <121341025+chase1281@users.noreply.github.com>
2025-10-02 11:56:12 +0900

    Update UserRepository.java

--- week3/src/main/java/com/example/BackendBeginner/domain/UserRepository.java
+++ week3/src/main/java/com/example/BackendBeginner/domain/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.BackendBeginner.controller.dto.UserRequestDTO;
 
 import java.util.*;
 
+//Factory 메서드 구조에 대해 알아보기.
 public interface UserRepository {
     User save(UserRequestDTO user);
     Optional<User> findById(Long id);
