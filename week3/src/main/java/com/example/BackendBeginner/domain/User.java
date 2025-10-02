ed7faac8b28f8533b3c0277d1cb4b457b71dcedb
MinJe Kim <121341025+chase1281@users.noreply.github.com>
2025-10-02 11:54:42 +0900

    Update User.java

--- week3/src/main/java/com/example/BackendBeginner/domain/User.java
+++ week3/src/main/java/com/example/BackendBeginner/domain/User.java
@@ -3,6 +3,7 @@ package com.example.BackendBeginner.domain;
 import lombok.Getter;
 
 @Getter
+// @NoArgsConstructor(access = AccessLevel.PROTECTED) 사용 시 기본 생성자 자동 생성
 public class User {
     private Long userId;
     private String username;
